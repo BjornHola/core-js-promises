37ef7dfe19ecefae5c4b047504560ac4837561f6
BjornHola <broadherstby@gmail.com>
2025-04-12 23:10:50 +0300

    init: fulfil task 5

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -179,9 +179,20 @@ getFirstPromiseResult([promise3, promise4, promise6])
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
+getAllOrNothing([Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)])
+  .then((result) => result)
+  .catch((error) => error);
+
+getAllOrNothing([
+  Promise.resolve(1),
+  Promise.reject(new Error('2')),
+  Promise.resolve(3),
+])
+  .then((result) => result)
+  .catch((error) => error);
 
 /**
  * Processes an array of promises and returns a promise that resolves with an array of their results.
